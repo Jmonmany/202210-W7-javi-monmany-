12e9d14d96a084db8e631fefa7abe29cf9ab7901
Javi <monmany1992@gmail.com>
2022-11-19 19:29:53 +0100

    Added clases and hooks

---
+++ src/Fighter.ts
@@ -0,0 +1,17 @@
+import { Character } from './Character';
+
+export class Fighter extends Character {
+    constructor(
+        name: string,
+        family: string,
+        age: number,
+        state: boolean,
+        message: string,
+        public weapon: string,
+        public dexterity: number
+    ) {
+        super(name, family, age, state, message);
+        state = true;
+        message = 'First I hit and then I ask';
+    }
+}
---
+++ src/Squire.ts
@@ -0,0 +1,16 @@
+import { Character } from './Character';
+
+export class Squire extends Character {
+    constructor(
+        name: string,
+        family: string,
+        age: number,
+        state: boolean,
+        message: string,
+        public serving: string,
+        public degreeOfSuckingUp: number
+    ) {
+        super(name, family, age, state, message);
+        message = "I'm a loser";
+    }
+}
